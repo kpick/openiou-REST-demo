88f716e5ea6718fd045e580fa0d64b9b312d6939
Ken Pickering <ken.pickering@gmail.com>
2014-06-09 16:47:18 -0400

    First Checkin

---
+++ app.js
@@ -0,0 +1,177 @@
+// Includes
+var restify = require('restify');
+var mongojs = require("mongojs");
+var path = require('path');
+
+// -----------------------------------------------------------------------------
+// VARIABLES
+// -----------------------------------------------------------------------------
+
+// Paths
+var informationPath = '/information';
+var purchasesPath = '/purchases';
+var salesPath = '/sales';
+var authorizePath = '/authorize';
+var statusPath = '/status';
+
+var debugUsersPath = '/debugusers';
+var debugTxnsPath = '/debugtxns';
+
+
+// Server Address/Port
+var addr = process.env.IP;
+var port = process.env.PORT;
+
+// -----------------------------------------------------------------------------
+// SET UP
+// -----------------------------------------------------------------------------
+
+// Mongo Setup
+var databaseUrl = addr + ':27017/openiou';
+var collections = ["users", "transactions"]
+var db = mongojs.connect(databaseUrl, collections);
+console.log("Mongo listening at " + databaseUrl);
+
+// REST API
+var serverRest = restify.createServer({
+    "name":"openiou"
+    });
+serverRest.listen(process.env.PORT ,addr, function(){
+    console.log('%s listening at %s ', serverRest.name , serverRest.url);
+});
+
+// RESTify Options
+serverRest.use(restify.queryParser());
+serverRest.use(restify.bodyParser());
+serverRest.use(restify.CORS());
+
+// -----------------------------------------------------------------------------
+// Define Endpoints
+// -----------------------------------------------------------------------------
+serverRest.get({path : '/' , version : '0.0.1'} , base);
+
+serverRest.post({path : informationPath +'/:uid' , version : '0.0.1'} , information);
+serverRest.get({path : informationPath +'/:uid' , version : '0.0.1'} , information);
+
+serverRest.post({path : purchasesPath +'/:uid' , version : '0.0.1'} , purchases);
+serverRest.get({path : purchasesPath +'/:uid' , version : '0.0.1'} , purchases);
+
+serverRest.post({path : salesPath +'/:uid' , version : '0.0.1'} , sales);
+serverRest.get({path : salesPath +'/:uid' , version : '0.0.1'} , sales);
+
+serverRest.post({path : authorizePath +'/:payer/:payee' , version : '0.0.1'} , authorize);
+
+// DEBUGGING
+serverRest.get({path : debugUsersPath , version : '0.0.1'} , debugUsers);
+serverRest.get({path : debugTxnsPath , version : '0.0.1'} , debugTxns);
+
+// -----------------------------------------------------------------------------
+// Methods
+// -----------------------------------------------------------------------------
+function base (req, res , next){
+    res.send(200 , "Welcome to OpenIOU");
+}
+
+function information(req, res , next){
+    db.users.find({_id:mongojs.ObjectId(req.params.uid)}).limit(1).sort({name : -1} , function(err , success){
+        if(success){
+            console.log('Response success '+ success);
+            res.send(200 , success);
+            return next();
+        }else{
+            console.log('Response error '+err);
+            return next(err);
+        }
+    });
+}
+
+function purchases (req, res , next) {
+    db.transactions.find({payer: req.params.uid}).sort({date : -1} , function(err , success){
+        if(success){
+            console.log('Response success '+ success);
+            res.send(200 , success);
+            return next();
+        }else{
+            console.log('Response error '+err);
+            return next(err);
+        }
+    });
+}
+
+function sales (req, res , next) {
+    db.transactions.find({payee: req.params.uid}).sort({date : -1} , function(err , success){
+        if(success){
+            console.log('Response success '+ success);
+            res.send(200 , success);
+            return next();
+        }else{
+            console.log('Response error '+err);
+            return next(err);
+        }
+    });   
+}
+
+function authorize (req, res , next) {
+    var txn = {};
+    
+    txn.date = new Date();
+    txn.payer = req.params.payer;
+    txn.payee = req.params.payee;
+    txn.approval = makeid(15);
+    txn.metadata = "Location: Natick Store, Amount: 592.24, Currency: USD";
+ 
+    db.transactions.save(txn , function(err , success){
+        console.log('Response success '+success);
+        console.log('Response error '+err);
+        if(success){
+            res.send(201 , txn);
+            return next();
+        }else{
+            return next(err);
+        }
+    });
+}
+
+
+// -----------------------------------------------------------------------------
+// UTIL
+// -----------------------------------------------------------------------------
+function makeid(size)
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < size; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+// -----------------------------------------------------------------------------
+// DEBUGGING
+// -----------------------------------------------------------------------------
+function debugTxns (req, res , next){
+    db.transactions.find().limit(20).sort({name : -1} , function(err , success){
+        if(success){
+            console.log('Response success '+success);
+            res.send(200 , success);
+            return next();
+        }else{
+            console.log('Response error '+err);
+            return next(err);
+        }
+    });
+}
+
+function debugUsers (req, res , next){
+    db.users.find().limit(20).sort({name : -1} , function(err , success){
+        if(success){
+            console.log('Response success '+success);
+            res.send(200 , success);
+            return next();
+        }else{
+            console.log('Response error '+err);
+            return next(err);
+        }
+    });
+}
---
+++ mongoinsert.js
@@ -0,0 +1,75 @@
+var addr = process.env.IP;
+var mongojs = require("mongojs");
+
+var databaseUrl = addr + ':27017/openiou';
+var collections = ["users", "transactions"]
+var db = mongojs.connect(databaseUrl, collections);
+/*
+db.users.save({
+    email: "ken.pickering@gmail.com",
+    type: "payer", 
+    cert: "-----BEGIN CERTIFICATE-----MIIBuTCCASKgAwIBAgIQNdNhtuV5GbNHYZsf+LvM0zANBgkqhkiG9w0BAQUFADAbMRkwFwYDVQQDExBFZGlkZXYgU21va2VUZXN0MB4XDTA4MTExMjE5NTEzNVoXDTM5MTIzMTIzNTk1OVowGzEZMBcGA1UEAxMQRWRpZGV2IFNtb2tlVGVzdDCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAm6zGzqxejwswWTNLcSsa7P8xqODspX9VQBuq5W1RoTgQ0LNR64+7ywLjH8+wrb/lB6QV7s2SFUiWDeduVesvMJkWtZ5zzQyl3iUaCBpT4S5AaO3/wkYQSKdI108pXH7Aue0e/ZOwgEEX1N6OaPQn7AmAB4uq1h+ffw+rRKNHqnsCAwEAATANBgkqhkiG9w0BAQUFAAOBgQCZmj+pgRsN6HpoICawK3XXNAmicgfQkailX9akIjD3xSCwEQx4nG6tZjTz30u4NoSffW7pch58SxuZQDqW5NsJcQNqNgo/dMoqqpXdi2/0BYEcJ8pjsngrFm+fM2BnyGpXH7aWuKsWjVFGlWlF+yi8I35Q8wFJt2Z/XGA7WWDjvw==-----END CERTIFICATE-----",
+    accounts: "Paypal, VISA, American Express, Square, Amazon, Google, Circle (Bitcoin)",
+    preferred_account: "Amazon"}
+    , function(err, saved) {
+  if( err || !saved ) console.log("User not saved");
+  else console.log("User saved");
+});
+
+db.users.save({
+    email: "tyson.kopczynski@taosage.net",
+    type: "payer",
+    cert: "-----BEGIN CERTIFICATE-----MIIBuTCCASKgAwIBAgIQNdNhtuV5GbNHYZsf+LvM0zANBgkqhkiG9w0BAQUFADAbMRkwFwYDVQQDExBFZGlkZXYgU21va2VUZXN0MB4XDTA4MTExMjE5NTEzNVoXDTM5MTIzMTIzNTk1OVowGzEZMBcGA1UEAxMQRWRpZGV2IFNtb2tlVGVzdDCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAm6zGzqxejwswWTNLcSsa7P8xqODspX9VQBuq5W1RoTgQ0LNR64+7ywLjH8+wrb/lB6QV7s2SFUiWDeduVesvMJkWtZ5zzQyl3iUaCBpT4S5AaO3/wkYQSKdI108pXH7Aue0e/ZOwgEEX1N6OaPQn7AmAB4uq1h+ffw+rRKNHqnsCAwEAATANBgkqhkiG9w0BAQUFAAOBgQCZmj+pgRsN6HpoICawK3XXNAmicgfQkailX9akIjD3xSCwEQx4nG6tZjTz30u4NoSffW7pch58SxuZQDqW5NsJcQNqNgo/dMoqqpXdi2/0BYEcJ8pjsngrFm+fM2BnyGpXH7aWuKsWjVFGlWlF+yi8I35Q8wFJt2Z/XGA7WWDjvw==-----END CERTIFICATE-----",
+    accounts: "VISA, American Express, Square, Amazon, Google",
+    preferred_account: "Square"}
+    , function(err, saved) {
+  if( err || !saved ) console.log("User not saved");
+  else console.log("User saved");
+});
+
+db.users.save({
+    email: "daniel.chiang@gmail.com",
+    type: "payer",
+    cert: "-----BEGIN CERTIFICATE-----MIIBuTCCASKgAwIBAgIQNdNhtuV5GbNHYZsf+LvM0zANBgkqhkiG9w0BAQUFADAbMRkwFwYDVQQDExBFZGlkZXYgU21va2VUZXN0MB4XDTA4MTExMjE5NTEzNVoXDTM5MTIzMTIzNTk1OVowGzEZMBcGA1UEAxMQRWRpZGV2IFNtb2tlVGVzdDCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAm6zGzqxejwswWTNLcSsa7P8xqODspX9VQBuq5W1RoTgQ0LNR64+7ywLjH8+wrb/lB6QV7s2SFUiWDeduVesvMJkWtZ5zzQyl3iUaCBpT4S5AaO3/wkYQSKdI108pXH7Aue0e/ZOwgEEX1N6OaPQn7AmAB4uq1h+ffw+rRKNHqnsCAwEAATANBgkqhkiG9w0BAQUFAAOBgQCZmj+pgRsN6HpoICawK3XXNAmicgfQkailX9akIjD3xSCwEQx4nG6tZjTz30u4NoSffW7pch58SxuZQDqW5NsJcQNqNgo/dMoqqpXdi2/0BYEcJ8pjsngrFm+fM2BnyGpXH7aWuKsWjVFGlWlF+yi8I35Q8wFJt2Z/XGA7WWDjvw==-----END CERTIFICATE-----",
+    accounts: "Paypal, VISA, Square, Amazon, Google, Circle (Bitcoin)",
+    preferred_account: "Paypal"}
+    , function(err, saved) {
+  if( err || !saved ) console.log("User not saved");
+  else console.log("User saved");
+});
+
+db.users.save({
+    email: "payments@apple.com",
+    type: "business",
+    cert: "-----BEGIN CERTIFICATE-----MIIBuTCCASKgAwIBAgIQNdNhtuV5GbNHYZsf+LvM0zANBgkqhkiG9w0BAQUFADAbMRkwFwYDVQQDExBFZGlkZXYgU21va2VUZXN0MB4XDTA4MTExMjE5NTEzNVoXDTM5MTIzMTIzNTk1OVowGzEZMBcGA1UEAxMQRWRpZGV2IFNtb2tlVGVzdDCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAm6zGzqxejwswWTNLcSsa7P8xqODspX9VQBuq5W1RoTgQ0LNR64+7ywLjH8+wrb/lB6QV7s2SFUiWDeduVesvMJkWtZ5zzQyl3iUaCBpT4S5AaO3/wkYQSKdI108pXH7Aue0e/ZOwgEEX1N6OaPQn7AmAB4uq1h+ffw+rRKNHqnsCAwEAATANBgkqhkiG9w0BAQUFAAOBgQCZmj+pgRsN6HpoICawK3XXNAmicgfQkailX9akIjD3xSCwEQx4nG6tZjTz30u4NoSffW7pch58SxuZQDqW5NsJcQNqNgo/dMoqqpXdi2/0BYEcJ8pjsngrFm+fM2BnyGpXH7aWuKsWjVFGlWlF+yi8I35Q8wFJt2Z/XGA7WWDjvw==-----END CERTIFICATE-----",
+    accounts: "Paypal, VISA, MasterCard, American Express, Discover, AppleID, Circle (Bitcoin)",
+    preferred_account: "AppleID"}
+    , function(err, saved) {
+  if( err || !saved ) console.log("User not saved");
+  else console.log("User saved");
+});
+*/
+db.transactions.save({
+    date: "2014-06-01T17:59:07.849Z",
+    payer : "539617a1decf51daabc9ed9e",
+    payee: "539617a1decf51daabc9eda1",
+    approval: "ZZjkshjds9017727",
+    metadata: "Location: Natick Store, Amount: 592.24, Currency: USD"
+    }
+    , function(err, saved) {
+  if( err || !saved ) console.log("Data not saved");
+  else console.log("Data saved");
+});
+
+db.transactions.save({
+    date: "2014-06-02T17:59:07.849Z",
+    payer : "539617a1decf51daabc9eda0",
+    payee: "539617a1decf51daabc9eda1",
+    approval: "ZZjkshjds901727",
+    metadata: "Location: Natick Store, Amount: 1992.24, Currency: USD"
+    }
+    , function(err, saved) {
+  if( err || !saved ) console.log("Data not saved");
+  else console.log("Data saved");
+});
+
